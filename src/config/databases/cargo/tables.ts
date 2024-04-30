32bc6db4c27ee53f55965c16df5440cf06e48741
Niclas Scheiber <niclas.scheiber@fb3.fra-uas.de>
2024-04-30 15:13:45 +0200

    Initial DM version
    - Bad schemes
    - No edges
    - No placement info

---
+++ src/config/databases/cargo/tables.ts
@@ -0,0 +1,297 @@
+import { TableConfig } from "../../../Visualizer/types";
+
+import embeddedObjectAWBUseIndicatorTable from "./tables/EmbeddedObject.AWBUseIndicator.json";
+import embeddedObjectAircraftPossibilityCodeTable from "./tables/EmbeddedObject.AircraftPossibilityCode.json";
+import embeddedObjectBasicRateClassCodeTable from "./tables/EmbeddedObject.BasicRateClassCode.json";
+import embeddedObjectChargeCodeTable from "./tables/EmbeddedObject.ChargeCode.json";
+import embeddedObjectChargeIdentifierTable from "./tables/EmbeddedObject.ChargeIdentifier.json";
+import embeddedObjectCommodityCodeTable from "./tables/EmbeddedObject.CommodityCode.json";
+import embeddedObjectCurrencyCodeTable from "./tables/EmbeddedObject.CurrencyCode.json";
+import embeddedObjectDangerousGoodsCodeTable from "./tables/EmbeddedObject.DangerousGoodsCode.json";
+import embeddedObjectDemurrageCodeTable from "./tables/EmbeddedObject.DemurrageCode.json";
+import embeddedObjectDimensionsUnitCodeTable from "./tables/EmbeddedObject.DimensionsUnitCode.json";
+import embeddedObjectEntitlementCodeTable from "./tables/EmbeddedObject.EntitlementCode.json";
+import embeddedObjectExplosiveCompatibilityGroupCodeTable from "./tables/EmbeddedObject.ExplosiveCompatibilityGroupCode.json";
+import embeddedObjectGoodsTypeCodeTable from "./tables/EmbeddedObject.GoodsTypeCode.json";
+import embeddedObjectGoodsTypeExtensionCodeTable from "./tables/EmbeddedObject.GoodsTypeExtensionCode.json";
+import embeddedObjectMeasurementUnitCodeTable from "./tables/EmbeddedObject.MeasurementUnitCode.json";
+import embeddedObjectModeCodeTable from "./tables/EmbeddedObject.ModeCode.json";
+import embeddedObjectMovementIndicatorTable from "./tables/EmbeddedObject.MovementIndicator.json";
+import embeddedObjectOtherChargeCodeTable from "./tables/EmbeddedObject.OtherChargeCode.json";
+import embeddedObjectPackageMarkCodeTable from "./tables/EmbeddedObject.PackageMarkCode.json";
+import embeddedObjectPackagingDangerLevelCodeTable from "./tables/EmbeddedObject.PackagingDangerLevelCode.json";
+import embeddedObjectParticipantIdentifierTable from "./tables/EmbeddedObject.ParticipantIdentifier.json";
+import embeddedObjectPrepaidCollectIndicatorTable from "./tables/EmbeddedObject.PrepaidCollectIndicator.json";
+import embeddedObjectRaTypeCodeTable from "./tables/EmbeddedObject.RaTypeCode.json";
+import embeddedObjectRadioactiveMaterialClassificationTable from "./tables/EmbeddedObject.RadioactiveMaterialClassification.json";
+import embeddedObjectRateClassCodeTable from "./tables/EmbeddedObject.RateClassCode.json";
+import embeddedObjectRatingsTypeTable from "./tables/EmbeddedObject.RatingsType.json";
+import embeddedObjectRegulatedEntityCategoryCodeTable from "./tables/EmbeddedObject.RegulatedEntityCategoryCode.json";
+import embeddedObjectScreeningExemptionTable from "./tables/EmbeddedObject.ScreeningExemption.json";
+import embeddedObjectScreeningMethodTable from "./tables/EmbeddedObject.ScreeningMethod.json";
+import embeddedObjectSecurityStatusTable from "./tables/EmbeddedObject.SecurityStatus.json";
+import embeddedObjectServiceCodeTable from "./tables/EmbeddedObject.ServiceCode.json";
+import embeddedObjectShipmentSecurityStatusTable from "./tables/EmbeddedObject.ShipmentSecurityStatus.json";
+import embeddedObjectSignatoryRoleTable from "./tables/EmbeddedObject.SignatoryRole.json";
+import embeddedObjectSignatureTypeCodeTable from "./tables/EmbeddedObject.SignatureTypeCode.json";
+import embeddedObjectSpecialHandlingCodeTable from "./tables/EmbeddedObject.SpecialHandlingCode.json";
+import embeddedObjectStatusCodeTable from "./tables/EmbeddedObject.StatusCode.json";
+import embeddedObjectTemperatureUnitCodeTable from "./tables/EmbeddedObject.TemperatureUnitCode.json";
+import embeddedObjectTransactionPurposeCodeTable from "./tables/EmbeddedObject.TransactionPurposeCode.json";
+import embeddedObjectTransportMeansServiceTypeTable from "./tables/EmbeddedObject.TransportMeansServiceType.json";
+import embeddedObjectULDChargeCodeTable from "./tables/EmbeddedObject.ULDChargeCode.json";
+import embeddedObjectULDConditionCodeTable from "./tables/EmbeddedObject.ULDConditionCode.json";
+import embeddedObjectULDLoadingIndicatorTable from "./tables/EmbeddedObject.ULDLoadingIndicator.json";
+import embeddedObjectVolumeUnitCodeTable from "./tables/EmbeddedObject.VolumeUnitCode.json";
+import embeddedObjectWeightUnitCodeTable from "./tables/EmbeddedObject.WeightUnitCode.json";
+import embeddedObjectActionTimeTypeTable from "./tables/EmbeddedObject.ActionTimeType.json";
+import embeddedObjectActivitySequenceTable from "./tables/EmbeddedObject.ActivitySequence.json";
+import embeddedObjectAddressTable from "./tables/EmbeddedObject.Address.json";
+import embeddedObjectCodeListElementTable from "./tables/EmbeddedObject.CodeListElement.json";
+import embeddedObjectAdjustmentsTable from "./tables/EmbeddedObject.Adjustments.json";
+import logisticsObjectAnswerTable from "./tables/LogisticsObject.Answer.json";
+import logisticsObjectActorTable from "./tables/LogisticsObject.Actor.json";
+import embeddedObjectValueTable from "./tables/EmbeddedObject.Value.json";
+import logisticsObjectPieceTable from "./tables/LogisticsObject.Piece.json";
+import logisticsObjectLocationTable from "./tables/LogisticsObject.Location.json";
+import logisticsObjectEpermitConsignmentTable from "./tables/LogisticsObject.EpermitConsignment.json";
+import logisticsObjectOrganizationTable from "./tables/LogisticsObject.Organization.json";
+import logisticsObjectIotDeviceTable from "./tables/LogisticsObject.IotDevice.json";
+import logisticsObjectBillingDetailsTable from "./tables/LogisticsObject.BillingDetails.json";
+import logisticsObjectBookingTable from "./tables/LogisticsObject.Booking.json";
+import logisticsObjectBookingOptionTable from "./tables/LogisticsObject.BookingOption.json";
+import embeddedObjectBookingPreferencesTable from "./tables/EmbeddedObject.BookingPreferences.json";
+import logisticsObjectBookingRequestTable from "./tables/LogisticsObject.BookingRequest.json";
+import logisticsObjectBookingShipmentTable from "./tables/LogisticsObject.BookingShipment.json";
+import embeddedObjectBookingStatusTable from "./tables/EmbeddedObject.BookingStatus.json";
+import embeddedObjectBookingTimesTable from "./tables/EmbeddedObject.BookingTimes.json";
+import logisticsObjectCarrierTable from "./tables/LogisticsObject.Carrier.json";
+import embeddedObjectCarrierProductTable from "./tables/EmbeddedObject.CarrierProduct.json";
+import logisticsObjectPersonTable from "./tables/LogisticsObject.Person.json";
+import logisticsObjectCheckTable from "./tables/LogisticsObject.Check.json";
+import logisticsObjectCheckTemplateTable from "./tables/LogisticsObject.CheckTemplate.json";
+import embeddedObjectCheckTotalResultTable from "./tables/EmbeddedObject.CheckTotalResult.json";
+import embeddedObjectCO2EmissionsTable from "./tables/EmbeddedObject.CO2Emissions.json";
+import logisticsObjectLoadingMaterialTable from "./tables/LogisticsObject.LoadingMaterial.json";
+import logisticsObjectComposingTable from "./tables/LogisticsObject.Composing.json";
+import embeddedObjectCompositionTypeTable from "./tables/EmbeddedObject.CompositionType.json";
+import logisticsObjectSensorTable from "./tables/LogisticsObject.Sensor.json";
+import logisticsObjectPieceLiveAnimalsTable from "./tables/LogisticsObject.PieceLiveAnimals.json";
+import embeddedObjectContactDetailTypeTable from "./tables/EmbeddedObject.ContactDetailType.json";
+import embeddedObjectContactDetailTable from "./tables/EmbeddedObject.ContactDetail.json";
+import embeddedObjectContactRoleTable from "./tables/EmbeddedObject.ContactRole.json";
+import logisticsObjectItemTable from "./tables/LogisticsObject.Item.json";
+import logisticsObjectDgProductRadioactiveTable from "./tables/LogisticsObject.DgProductRadioactive.json";
+import logisticsObjectProductTable from "./tables/LogisticsObject.Product.json";
+import logisticsObjectCustomsInformationTable from "./tables/LogisticsObject.CustomsInformation.json";
+import embeddedObjectCurrencyValueTable from "./tables/EmbeddedObject.CurrencyValue.json";
+import logisticsObjectWaybillTable from "./tables/LogisticsObject.Waybill.json";
+import logisticsObjectDgDeclarationTable from "./tables/LogisticsObject.DgDeclaration.json";
+import embeddedObjectDimensionsTable from "./tables/EmbeddedObject.Dimensions.json";
+import embeddedObjectDirectionTypeTable from "./tables/EmbeddedObject.DirectionType.json";
+import logisticsObjectExternalReferenceTable from "./tables/LogisticsObject.ExternalReference.json";
+import logisticsObjectLiveAnimalsEpermitTable from "./tables/LogisticsObject.LiveAnimalsEpermit.json";
+import embeddedObjectEventTimeTypeTable from "./tables/EmbeddedObject.EventTimeType.json";
+import embeddedObjectExecutionStatusTable from "./tables/EmbeddedObject.ExecutionStatus.json";
+import logisticsObjectBookingOptionRequestTable from "./tables/LogisticsObject.BookingOptionRequest.json";
+import logisticsObjectPriceTable from "./tables/LogisticsObject.Price.json";
+import logisticsObjectProductDgTable from "./tables/LogisticsObject.ProductDg.json";
+import embeddedObjectGeolocationTable from "./tables/EmbeddedObject.Geolocation.json";
+import logisticsObjectUnitCompositionTable from "./tables/LogisticsObject.UnitComposition.json";
+import logisticsObjectInsuranceTable from "./tables/LogisticsObject.Insurance.json";
+import logisticsObjectShipmentTable from "./tables/LogisticsObject.Shipment.json";
+import embeddedObjectPartyTable from "./tables/EmbeddedObject.Party.json";
+import logisticsObjectDgRadioactiveIsotopeTable from "./tables/LogisticsObject.DgRadioactiveIsotope.json";
+import embeddedObjectLoadTypeTable from "./tables/EmbeddedObject.LoadType.json";
+import logisticsObjectLoadingUnitTable from "./tables/LogisticsObject.LoadingUnit.json";
+import logisticsObjectLoadingTable from "./tables/LogisticsObject.Loading.json";
+import embeddedObjectLoadingTypeTable from "./tables/EmbeddedObject.LoadingType.json";
+import embeddedObjectMeasurementTable from "./tables/EmbeddedObject.Measurement.json";
+import embeddedObjectModeQualifierTable from "./tables/EmbeddedObject.ModeQualifier.json";
+import embeddedObjectMovementTimeTypeTable from "./tables/EmbeddedObject.MovementTimeType.json";
+import embeddedObjectMovementTimeTable from "./tables/EmbeddedObject.MovementTime.json";
+import logisticsObjectTransportMeansTable from "./tables/LogisticsObject.TransportMeans.json";
+import logisticsObjectTransportMovementTable from "./tables/LogisticsObject.TransportMovement.json";
+import logisticsObjectCompanyTable from "./tables/LogisticsObject.Company.json";
+import embeddedObjectCharacteristicTable from "./tables/EmbeddedObject.Characteristic.json";
+import embeddedObjectOtherChargeTable from "./tables/EmbeddedObject.OtherCharge.json";
+import embeddedObjectOtherIdentifierTable from "./tables/EmbeddedObject.OtherIdentifier.json";
+import embeddedObjectRegulatedEntityTable from "./tables/EmbeddedObject.RegulatedEntity.json";
+import logisticsObjectPackagingTypeTable from "./tables/LogisticsObject.PackagingType.json";
+import embeddedObjectPieceGroupTable from "./tables/EmbeddedObject.PieceGroup.json";
+import logisticsObjectQuestionTable from "./tables/LogisticsObject.Question.json";
+import embeddedObjectRangesTable from "./tables/EmbeddedObject.Ranges.json";
+import embeddedObjectRatingsTable from "./tables/EmbeddedObject.Ratings.json";
+import embeddedObjectSecurityDeclarationTable from "./tables/EmbeddedObject.SecurityDeclaration.json";
+import embeddedObjectSensorTypeTable from "./tables/EmbeddedObject.SensorType.json";
+import embeddedObjectEpermitSignatureTable from "./tables/EmbeddedObject.EpermitSignature.json";
+import embeddedObjectStationRemarksTable from "./tables/EmbeddedObject.StationRemarks.json";
+import embeddedObjectBookingOptionStatusTable from "./tables/EmbeddedObject.BookingOptionStatus.json";
+import embeddedObjectStoringTable from "./tables/EmbeddedObject.Storing.json";
+import embeddedObjectStoringTypeTable from "./tables/EmbeddedObject.StoringType.json";
+import embeddedObjectTemperatureInstructionsTable from "./tables/EmbeddedObject.TemperatureInstructions.json";
+import embeddedObjectVolumetricWeightTable from "./tables/EmbeddedObject.VolumetricWeight.json";
+import embeddedObjectTransportLegsTable from "./tables/EmbeddedObject.TransportLegs.json";
+import embeddedObjectUnitsPreferenceTable from "./tables/EmbeddedObject.UnitsPreference.json";
+import embeddedObjectWaybillLineItemTable from "./tables/EmbeddedObject.WaybillLineItem.json";
+import embeddedObjectWaybillTypeTable from "./tables/EmbeddedObject.WaybillType.json";
+import embeddedObjectPieceDgTable from "./tables/EmbeddedObject.PieceDg.json";
+import embeddedObjectItemDgTable from "./tables/EmbeddedObject.ItemDg.json";
+import embeddedObjectLoosePieceTable from "./tables/EmbeddedObject.LoosePiece.json";
+import embeddedObjectNonHumanActorTable from "./tables/EmbeddedObject.NonHumanActor.json";
+import embeddedObjectPublicAuthorityTable from "./tables/EmbeddedObject.PublicAuthority.json";
+import embeddedObjectStorageTable from "./tables/EmbeddedObject.Storage.json";
+import embeddedObjectULDTable from "./tables/EmbeddedObject.ULD.json";
+import embeddedObjectULDBasicPieceTable from "./tables/EmbeddedObject.ULDBasicPiece.json";
+import embeddedObjectULDSpecificPieceTable from "./tables/EmbeddedObject.ULDSpecificPiece.json";
+import embeddedObjectVolumePieceGroupTable from "./tables/EmbeddedObject.VolumePieceGroup.json";
+
+const tables: TableConfig[] = [
+  embeddedObjectAWBUseIndicatorTable,
+  embeddedObjectAircraftPossibilityCodeTable,
+  embeddedObjectBasicRateClassCodeTable,
+  embeddedObjectChargeCodeTable,
+  embeddedObjectChargeIdentifierTable,
+  embeddedObjectCommodityCodeTable,
+  embeddedObjectCurrencyCodeTable,
+  embeddedObjectDangerousGoodsCodeTable,
+  embeddedObjectDemurrageCodeTable,
+  embeddedObjectDimensionsUnitCodeTable,
+  embeddedObjectEntitlementCodeTable,
+  embeddedObjectExplosiveCompatibilityGroupCodeTable,
+  embeddedObjectGoodsTypeCodeTable,
+  embeddedObjectGoodsTypeExtensionCodeTable,
+  embeddedObjectMeasurementUnitCodeTable,
+  embeddedObjectModeCodeTable,
+  embeddedObjectMovementIndicatorTable,
+  embeddedObjectOtherChargeCodeTable,
+  embeddedObjectPackageMarkCodeTable,
+  embeddedObjectPackagingDangerLevelCodeTable,
+  embeddedObjectParticipantIdentifierTable,
+  embeddedObjectPrepaidCollectIndicatorTable,
+  embeddedObjectRaTypeCodeTable,
+  embeddedObjectRadioactiveMaterialClassificationTable,
+  embeddedObjectRateClassCodeTable,
+  embeddedObjectRatingsTypeTable,
+  embeddedObjectRegulatedEntityCategoryCodeTable,
+  embeddedObjectScreeningExemptionTable,
+  embeddedObjectScreeningMethodTable,
+  embeddedObjectSecurityStatusTable,
+  embeddedObjectServiceCodeTable,
+  embeddedObjectShipmentSecurityStatusTable,
+  embeddedObjectSignatoryRoleTable,
+  embeddedObjectSignatureTypeCodeTable,
+  embeddedObjectSpecialHandlingCodeTable,
+  embeddedObjectStatusCodeTable,
+  embeddedObjectTemperatureUnitCodeTable,
+  embeddedObjectTransactionPurposeCodeTable,
+  embeddedObjectTransportMeansServiceTypeTable,
+  embeddedObjectULDChargeCodeTable,
+  embeddedObjectULDConditionCodeTable,
+  embeddedObjectULDLoadingIndicatorTable,
+  embeddedObjectVolumeUnitCodeTable,
+  embeddedObjectWeightUnitCodeTable,
+  embeddedObjectActionTimeTypeTable,
+  embeddedObjectActivitySequenceTable,
+  embeddedObjectAddressTable,
+  embeddedObjectCodeListElementTable,
+  embeddedObjectAdjustmentsTable,
+  logisticsObjectAnswerTable,
+  logisticsObjectActorTable,
+  embeddedObjectValueTable,
+  logisticsObjectPieceTable,
+  logisticsObjectLocationTable,
+  logisticsObjectEpermitConsignmentTable,
+  logisticsObjectOrganizationTable,
+  logisticsObjectIotDeviceTable,
+  logisticsObjectBillingDetailsTable,
+  logisticsObjectBookingTable,
+  logisticsObjectBookingOptionTable,
+  embeddedObjectBookingPreferencesTable,
+  logisticsObjectBookingRequestTable,
+  logisticsObjectBookingShipmentTable,
+  embeddedObjectBookingStatusTable,
+  embeddedObjectBookingTimesTable,
+  logisticsObjectCarrierTable,
+  embeddedObjectCarrierProductTable,
+  logisticsObjectPersonTable,
+  logisticsObjectCheckTable,
+  logisticsObjectCheckTemplateTable,
+  embeddedObjectCheckTotalResultTable,
+  embeddedObjectCO2EmissionsTable,
+  logisticsObjectLoadingMaterialTable,
+  logisticsObjectComposingTable,
+  embeddedObjectCompositionTypeTable,
+  logisticsObjectSensorTable,
+  logisticsObjectPieceLiveAnimalsTable,
+  embeddedObjectContactDetailTypeTable,
+  embeddedObjectContactDetailTable,
+  embeddedObjectContactRoleTable,
+  logisticsObjectItemTable,
+  logisticsObjectDgProductRadioactiveTable,
+  logisticsObjectProductTable,
+  logisticsObjectCustomsInformationTable,
+  embeddedObjectCurrencyValueTable,
+  logisticsObjectWaybillTable,
+  logisticsObjectDgDeclarationTable,
+  embeddedObjectDimensionsTable,
+  embeddedObjectDirectionTypeTable,
+  logisticsObjectExternalReferenceTable,
+  logisticsObjectLiveAnimalsEpermitTable,
+  embeddedObjectEventTimeTypeTable,
+  embeddedObjectExecutionStatusTable,
+  logisticsObjectBookingOptionRequestTable,
+  logisticsObjectPriceTable,
+  logisticsObjectProductDgTable,
+  embeddedObjectGeolocationTable,
+  logisticsObjectUnitCompositionTable,
+  logisticsObjectInsuranceTable,
+  logisticsObjectShipmentTable,
+  embeddedObjectPartyTable,
+  logisticsObjectDgRadioactiveIsotopeTable,
+  embeddedObjectLoadTypeTable,
+  logisticsObjectLoadingUnitTable,
+  logisticsObjectLoadingTable,
+  embeddedObjectLoadingTypeTable,
+  embeddedObjectMeasurementTable,
+  embeddedObjectModeQualifierTable,
+  embeddedObjectMovementTimeTypeTable,
+  embeddedObjectMovementTimeTable,
+  logisticsObjectTransportMeansTable,
+  logisticsObjectTransportMovementTable,
+  logisticsObjectCompanyTable,
+  embeddedObjectCharacteristicTable,
+  embeddedObjectOtherChargeTable,
+  embeddedObjectOtherIdentifierTable,
+  embeddedObjectRegulatedEntityTable,
+  logisticsObjectPackagingTypeTable,
+  embeddedObjectPieceGroupTable,
+  logisticsObjectQuestionTable,
+  embeddedObjectRangesTable,
+  embeddedObjectRatingsTable,
+  embeddedObjectSecurityDeclarationTable,
+  embeddedObjectSensorTypeTable,
+  embeddedObjectEpermitSignatureTable,
+  embeddedObjectStationRemarksTable,
+  embeddedObjectBookingOptionStatusTable,
+  embeddedObjectStoringTable,
+  embeddedObjectStoringTypeTable,
+  embeddedObjectTemperatureInstructionsTable,
+  embeddedObjectVolumetricWeightTable,
+  embeddedObjectTransportLegsTable,
+  embeddedObjectUnitsPreferenceTable,
+  embeddedObjectWaybillLineItemTable,
+  embeddedObjectWaybillTypeTable,
+  embeddedObjectPieceDgTable,
+  embeddedObjectItemDgTable,
+  embeddedObjectLoosePieceTable,
+  embeddedObjectNonHumanActorTable,
+  embeddedObjectPublicAuthorityTable,
+  embeddedObjectStorageTable,
+  embeddedObjectULDTable,
+  embeddedObjectULDBasicPieceTable,
+  embeddedObjectULDSpecificPieceTable,
+  embeddedObjectVolumePieceGroupTable
+];
+
+export default tables;
