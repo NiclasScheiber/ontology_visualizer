ae9d10bbfb5131f021794f49d76d1eec311b16f9
aloccid-iata <140411726+aloccid-iata@users.noreply.github.com>
2024-05-01 17:47:05 +0200

    test

--- src/Visualizer/components/TableNode.tsx
+++ src/Visualizer/components/TableNode.tsx
@@ -68,9 +68,8 @@ export const TableNode: FC<NodeProps> = ({ data }) => {
             onMouseLeave={() => setSelectedColumn("")}
             onClick={() => {              
               window.requestAnimationFrame(() => {
-                const fitNodes: Node[] = [
-                  { id: 'Generic.booking', data: {}, position: { x: 5, y: 5 } }
-                ];
+                const fitNodes: Node = 
+                  { id: 'Generic.booking', data: {}, position: { x: 5, y: 5 } };
                 
                 const fitViewOptions: FitViewOptions = {
                   duration: 500,
