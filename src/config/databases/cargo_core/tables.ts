ef1a82d5f308253f7aeda816b3fb9280837b7ec9
Niclas Scheiber <niclas.scheiber@fb3.fra-uas.de>
2024-05-06 12:40:23 +0200

    Updated Cargo_Core for Jumps

--- src/config/databases/cargo_core/tables.ts
+++ src/config/databases/cargo_core/tables.ts
@@ -1,69 +1,65 @@
 import { TableConfig } from "../../../Visualizer/types";
 
 import embeddedActivitySequenceTable from "./tables/Embedded.ActivitySequence.json";
-import embeddedAddressTable from "./tables/Embedded.Address.json";
-import embeddedCodeListElementTable from "./tables/Embedded.CodeListElement.json";
 import abstractActorTable from "./tables/Abstract.Actor.json";
-import embeddedValueTable from "./tables/Embedded.Value.json";
-import corePieceTable from "./tables/Core.Piece.json";
-import commonLocationTable from "./tables/Common.Location.json";
-import abstractOrganizationTable from "./tables/Abstract.Organization.json";
+import embeddedAddressTable from "./tables/Embedded.Address.json";
 import serviceBookingTable from "./tables/Service.Booking.json";
-import commonPersonTable from "./tables/Common.Person.json";
+import embeddedCharacteristicTable from "./tables/Embedded.Characteristic.json";
+import embeddedCodeListElementTable from "./tables/Embedded.CodeListElement.json";
 import actionComposingTable from "./tables/Action.Composing.json";
 import embeddedContactDetailTable from "./tables/Embedded.ContactDetail.json";
-import coreItemTable from "./tables/Core.Item.json";
-import coreProductTable from "./tables/Core.Product.json";
 import embeddedCurrencyValueTable from "./tables/Embedded.CurrencyValue.json";
-import coreWaybillTable from "./tables/Core.Waybill.json";
 import embeddedDimensionsTable from "./tables/Embedded.Dimensions.json";
 import commonExternalReferenceTable from "./tables/Common.ExternalReference.json";
-import eventLogisticsEventTable from "./tables/Event.LogisticsEvent.json";
-import activityUnitCompositionTable from "./tables/Activity.UnitComposition.json";
-import coreShipmentTable from "./tables/Core.Shipment.json";
-import embeddedPartyTable from "./tables/Embedded.Party.json";
+import coreItemTable from "./tables/Core.Item.json";
 import actionLoadingTable from "./tables/Action.Loading.json";
+import commonLocationTable from "./tables/Common.Location.json";
+import eventLogisticsEventTable from "./tables/Event.LogisticsEvent.json";
+import abstractOrganizationTable from "./tables/Abstract.Organization.json";
+import embeddedOtherIdentifierTable from "./tables/Embedded.OtherIdentifier.json";
+import embeddedPartyTable from "./tables/Embedded.Party.json";
+import commonPersonTable from "./tables/Common.Person.json";
+import corePieceTable from "./tables/Core.Piece.json";
+import coreProductTable from "./tables/Core.Product.json";
+import coreShipmentTable from "./tables/Core.Shipment.json";
 import coreTransportMeansTable from "./tables/Core.TransportMeans.json";
 import activityTransportMovementTable from "./tables/Activity.TransportMovement.json";
-import embeddedCharacteristicTable from "./tables/Embedded.Characteristic.json";
-import embeddedOtherIdentifierTable from "./tables/Embedded.OtherIdentifier.json";
-import embeddedWaybillLineItemTable from "./tables/Embedded.WaybillLineItem.json";
-import commonNonHumanActorTable from "./tables/Common.NonHumanActor.json";
-import commonPublicAuthorityTable from "./tables/Common.PublicAuthority.json";
 import coreULDTable from "./tables/Core.ULD.json";
+import activityUnitCompositionTable from "./tables/Activity.UnitComposition.json";
+import embeddedValueTable from "./tables/Embedded.Value.json";
+import coreWaybillTable from "./tables/Core.Waybill.json";
+import embeddedWaybillLineItemTable from "./tables/Embedded.WaybillLineItem.json";
 
 const tables: TableConfig[] = [
   embeddedActivitySequenceTable,
-  embeddedAddressTable,
-  embeddedCodeListElementTable,
   abstractActorTable,
-  embeddedValueTable,
-  corePieceTable,
-  commonLocationTable,
-  abstractOrganizationTable,
+  embeddedAddressTable,
   serviceBookingTable,
-  commonPersonTable,
+  embeddedCharacteristicTable,
+  embeddedCodeListElementTable,
   actionComposingTable,
   embeddedContactDetailTable,
-  coreItemTable,
-  coreProductTable,
   embeddedCurrencyValueTable,
-  coreWaybillTable,
   embeddedDimensionsTable,
   commonExternalReferenceTable,
-  eventLogisticsEventTable,
-  activityUnitCompositionTable,
-  coreShipmentTable,
-  embeddedPartyTable,
+  coreItemTable,
   actionLoadingTable,
+  commonLocationTable,
+  eventLogisticsEventTable,
+  abstractOrganizationTable,
+  embeddedOtherIdentifierTable,
+  embeddedPartyTable,
+  commonPersonTable,
+  corePieceTable,
+  coreProductTable,
+  coreShipmentTable,
   coreTransportMeansTable,
   activityTransportMovementTable,
-  embeddedCharacteristicTable,
-  embeddedOtherIdentifierTable,
-  embeddedWaybillLineItemTable,
-  commonNonHumanActorTable,
-  commonPublicAuthorityTable,
-  coreULDTable
+  coreULDTable,
+  activityUnitCompositionTable,
+  embeddedValueTable,
+  coreWaybillTable,
+  embeddedWaybillLineItemTable
 ];
 
 export default tables;
