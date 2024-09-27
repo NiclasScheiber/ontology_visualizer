98ccca7946435db7493b4f0dbcbec2b3ad9452a0
Niclas Scheiber <niclas.scheiber@fb3.fra-uas.de>
2024-09-27 10:28:24 +0200

    Test for moving sub-dm

---
+++ src/config/databases/moving_pilot/tables.ts
@@ -0,0 +1,29 @@
+import { TableConfig } from "../../../Visualizer/types";
+
+import embeddedAddressTable from "./tables/Embedded.Address.json";
+import embeddedDimensionsTable from "./tables/Embedded.Dimensions.json";
+import commonExternalReferenceTable from "./tables/Common.ExternalReference.json";
+import commonLocationTable from "./tables/Common.Location.json";
+import eventLogisticsEventTable from "./tables/Event.LogisticsEvent.json";
+import abstractLogisticsServiceTable from "./tables/Abstract.LogisticsService.json";
+import agentOrganizationTable from "./tables/Agent.Organization.json";
+import embeddedPartyTable from "./tables/Embedded.Party.json";
+import coreShipmentTable from "./tables/Core.Shipment.json";
+import activityTransportMovementTable from "./tables/Activity.TransportMovement.json";
+import embeddedValueTable from "./tables/Embedded.Value.json";
+
+const tables: TableConfig[] = [
+  embeddedAddressTable,
+  embeddedDimensionsTable,
+  commonExternalReferenceTable,
+  commonLocationTable,
+  eventLogisticsEventTable,
+  abstractLogisticsServiceTable,
+  agentOrganizationTable,
+  embeddedPartyTable,
+  coreShipmentTable,
+  activityTransportMovementTable,
+  embeddedValueTable
+];
+
+export default tables;
